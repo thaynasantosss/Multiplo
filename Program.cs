544e47ac6ccf7f219ff5c3d4eb0eadcacdea68bc
thaynasantosss <thayna.2015.09@gmail.com>
2023-04-25 21:53:52 -0300

    tarefa concluída

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("*** Múltiplo ***");
+Console.WriteLine();
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Digite um número: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine();
+Console.Write("Avaliar se é múltiplo de: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (n1 % n2 == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"\n{n1} é múltiplo de {n2}");
+    Console.ResetColor();
+
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"\n{n1} não é múltiplo de {n2}");
+    Console.ResetColor();
+
+}
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine();
+Console.WriteLine("Clique em alguma tecla para ver outros múltiplos...");
+Console.ReadKey();
+Console.ResetColor();
+Console.Clear();
